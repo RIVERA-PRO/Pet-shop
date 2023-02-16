9949c432448095eeb8e758d7a0e1e66e61dd9c4b
Karen <karen.perez.0395@gmail.com>
2023-02-15 21:22:02 -0300

    cards terminadas

--- assets/module/functions.js
+++ assets/module/functions.js
@@ -29,27 +29,16 @@ export function filtradoDeFarmacia( objetos,  paginas) {
   }
 
 
-  export function allCards(events) {
-    return `<div class="card p-3 m-1 bg-dark" style="width: 18rem">
-      <img
-        src= ${events.imagen}
-        class="card-img-top"
-        style="height: 10rem"
-        alt="cine"
-      />
-      <div class="card-body d-flex flex-column justify-content-around">
-              <h5 class="card-title text-white fs-4">${events.name}</h5>
-              <p class="card-text text-white fs-5"> ${events.date} </p>
-              <p class="card-text text-white fs-5">
-                ${events.descripcion}
-              </p>
-              <p class="card-text text-white fs-5">
-                ${events.disponible}
-              </p>
-              <p class="card-text text-white fs-5"> Price: $${events.price} </p>
-              <a href="../assets/details.html?id=${events._id}&name=${events.name}" class="btn bg-dark-subtle d-flex justify-content-end " style="width: 4.5rem;"
-                >Details</a
-              >
-            </div>
-      </div>`;
+  export function allCards(objeto) {
+    return `<div class="card p-2 cover"  style="width: 20rem;">
+    <img src=${objeto.imagen} alt="${objeto.producto}" class="card-img-top" style="height: 15rem;">
+    <div class="card-body ">
+      <h5 class="card-title h-50 title">${objeto.producto}</h5>
+      <p class="m=0">Disponibles: ${objeto.disponibles}</p>
+      <div class="d-flex justify-content-evenly align-items-center">         
+        <p class="m-0">$${objeto.precio}</p>
+        <a href="./details.html?_id=${objeto._id}&name=${objeto.name}" class="btn btn-outline-primary align-self-end">Agregar al carrito</a>
+      </div>
+    </div>
+    </div>`
   }
